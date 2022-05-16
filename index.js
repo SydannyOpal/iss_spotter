770e418779c372810df14088c7dae49bfc09adde
= <sydanny15@gmail.com>
2022-05-16 09:55:33 -0400

    implementing iss IV

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const request = require("request");
-const { calculateMac } = require("request/lib/hawk");
-const myFun = require("./iss");
-const ipAdd, coords;
+// const request = require("request");
+// const { calculateMac } = require("request/lib/hawk");
+// const myFun = require("./iss");
+// const ipAdd, coords;
 
-const fetchMyIP = (error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+// const fetchMyIP = (error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   ipAdd = ip;
+//   console.log("It worked! Returned IP:", ip);
+//   myFun.fetchCoordsByIP(ipAdd);
+// };
+// myFun.fetchMyIP(fetchMyIP);
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  ipAdd = ip;
-  console.log("It worked! Returned IP:", ip);
-  myFun.fetchCoordsByIP(ipAdd);
 };
-myFun.fetchMyIP(fetchMyIP);
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  console.log(passTimes);
+});
--- iss.js
+++ iss.js
@@ -54,4 +54,27 @@ const fetchISSFlyOverTimes = function (lat, lng) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+const  nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+}
+
+module.exports = { nextISSTimesForMyLocation };
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
